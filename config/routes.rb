1643ed8bf1909e2dc45606bcca2fad24122752d0
Rudy Pena <rudypenajr@gmail.com>
2013-03-12 21:11:15 -0500

    added _heder, _Footer, about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Omrails::Application.routes.draw do
   get 'about' => 'pages#about'
-  root :to => "pages/home"
+
+  root :to => "pages#home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
